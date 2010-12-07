e586e6b9dcb7c5cdcbd5d12249908a273bfa5407
John Stone <john@cephal.com>
2010-12-06 19:30:58 -0500

    update

--- genRandomInt.py
+++ genRandomInt.py
@@ -1,22 +1,12 @@
-from random import randint, choice
+from datetime import date
+from datetime import timedelta
+from random import randint
 from inputs.iSUMOe6Random import iSUMOe6Random
 
 d = iSUMOe6Random()
-assetIds = [x.id for x in d.assets]
-taskIds = [x.id for x in d.tasks]
-
-initial_total_min = 10
-initial_total_max = 20
-initial_year_min = 1998
-initial_year_max = 2009
-initial_month_min = 1
-initial_month_max = 12
-initial_day_min = 1
-initial_day_max = 27
 
 source = open('inputs/iSUMOe6Random.py', 'r')
 f = open('iSUMOe6Random.py', 'w')
-
 
 line = source.readline()
  
@@ -25,10 +15,12 @@ while line:
     if not line.strip().startswith('schedule.force'): f.write(line)
     line = source.readline()
 
-
-for x in range(randint(initial_total_min, initial_total_max)):
-    schedule = "datetime("+str(randint(initial_year_min,initial_year_max))+", "+str(randint(initial_month_min,initial_month_max))+", "+str(randint(initial_day_min,initial_day_max))+")"
-    f.write("\t\tschedule.force(assets["+str(choice(assetIds))+"], tasks["+str(choice(taskIds))+"], DateRange("+schedule+", "+schedule+"))\n")
+for t in d.tasks:
+    if t.interval:
+        for a in d.assets:
+            schedule = date.today() - timedelta(days=randint(0,t.interval)) #random date
+            schedule = "datetime("+str(schedule.year)+", "+str(schedule.month)+", "+str(schedule.day)+")"
+            f.write("\t\tschedule.force(assets["+str(a.id)+"], tasks["+str(t.id)+"], DateRange("+schedule+", "+schedule+"))\n")
 
 while line:
     f.write(line)
